ced56076e1a15edc4e10272f126b5e7a00809b78
wahin <wahi08@gmail.com>
2020-04-07 16:49:52 -0400

    Angular version and material library
    Upgraded from Angular 2 to Angular 5
    Changed the table to angular material table
    Added sorting, pagination and filtering features

--- ms-test/src/app/app.component.ts
+++ ms-test/src/app/app.component.ts
@@ -1,21 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild,AfterViewInit  } from '@angular/core';
+import {MatTableDataSource, MatSort, MatPaginator} from '@angular/material';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  public groups :any = [
-    {
-      "initialMargins": 
-        [
-          {exchange:"CCL",currency:"USD",imNumber:"701,372.00",imInBase:"701,372.00"}
-          ,{exchange:"SFE",currency:"AUD",imNumber:"296,400.00",imInBase:"212,770.74"}
-          ,{exchange:"MON",currency:"CAD",imNumber:"86,903.00",imInBase:"66,910.22"}
-          ,{exchange:"ERX",currency:"EUR",imNumber:"732,967.00",imInBase:"852,588.02"}
-          ,{exchange:"JSC",currency:"JPY",imNumber:"780,000.00",imInBase:"7,012.18"}
-        ]
+export class AppComponent implements OnInit{
+  dataSource;
+  displayedColumns = [];
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+   /**
+  * Pre-defined columns list for user table
+  */
+  columnNamesinitialMargins = [{id: "exchange",value: "EXCHANGE"}, { id: "currency",value: "CURRENCY"},{id: "imNumber",value: "INITIAL MARGIN"},{id: "imInBase",value: "USD EQUIVALENT"}];
+
+  ngOnInit() {
+    this.displayedColumns = this.columnNamesinitialMargins.map(x => x.id);
+    this.createTable();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
     }
-  ]
+    
+  createTable() {
+    let groups: any = [
+      {
+        "initialMargins": 
+          [
+            {exchange:"CCL",currency:"USD",imNumber:"701,372.00",imInBase:"701,372.00"}
+            ,{exchange:"SFE",currency:"AUD",imNumber:"296,400.00",imInBase:"212,770.74"}
+            ,{exchange:"MON",currency:"CAD",imNumber:"86,903.00",imInBase:"66,910.22"}
+            ,{exchange:"ERX",currency:"EUR",imNumber:"732,967.00",imInBase:"852,588.02"}
+            ,{exchange:"JSC",currency:"JPY",imNumber:"780,000.00",imInBase:"7,012.18"}
+          ]
+      }
+    ]
+    this.dataSource = new MatTableDataSource(groups[0].initialMargins);
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
--- ms-test/src/app/app.module.ts
+++ ms-test/src/app/app.module.ts
@@ -1,19 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { MatTableModule,MatSortModule, MatPaginatorModule,MatFormFieldModule,MatInputModule } from '@angular/material';
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
