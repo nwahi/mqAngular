ed3d3d12a64b92467d6f695bfd084763d7f44a5c
Rahul Kargwal <rahul.kargwal@elevateservices.com>
2020-04-06 20:50:04 -0400

    Initial Margin data

--- ms-test/src/app/app.component.ts
+++ ms-test/src/app/app.component.ts
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
-  public groups :any = [{"name": "pencils", "items": ["red pencil","blue pencil","yellow pencil"]},{"name": "rubbers", "items": ["big rubber","small rubber"]}];
+  public groups :any = [
+    {
+      "initialMargins": 
+        [
+          {exchange:"CCL",currency:"USD",imNumber:"701,372.00",imInBase:"701,372.00"}
+          ,{exchange:"SFE",currency:"AUD",imNumber:"296,400.00",imInBase:"212,770.74"}
+          ,{exchange:"MON",currency:"CAD",imNumber:"86,903.00",imInBase:"66,910.22"}
+          ,{exchange:"ERX",currency:"EUR",imNumber:"732,967.00",imInBase:"852,588.02"}
+          ,{exchange:"JSC",currency:"JPY",imNumber:"780,000.00",imInBase:"7,012.18"}
+        ]
+    }
+  ]
 }
